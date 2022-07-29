dec3ac363ab30c6782090dfa97c3ce7c31dc517c
Lars <lars@roth-kl.de>
2022-07-29 09:31:44 +0200

    Remove setup script

--- customize.sh
+++
@@ -1,5 +0,0 @@
-FULL_NAME="Lars Roth"
-GITHUB_USER="Supporterino"
-REPO_NAME="alertmanager-api-v2"
-sed -i.mybak "s/ryansonshine/$GITHUB_USER/g; s/typescript-npm-package-template\|my-package-name/$REPO_NAME/g; s/Ryan Sonshine/$FULL_NAME/g" package.json package-lock.json README.md
-rm *.mybak
