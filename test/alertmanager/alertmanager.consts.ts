dc9b3c31a636c098a97cf81313fab92df080dab6
Lars <lars@roth-kl.de>
2022-08-01 10:44:09 +0200

    test(alertmanager): setup axios mock for writing api tests. and implement first test for `/status`

---
+++ test/alertmanager/alertmanager.consts.ts
@@ -0,0 +1,75 @@
+export const statusResponse = {
+  uptime: '2022-06-23T07:15:15.759Z',
+  config: {
+    original:
+      'global:\n  resolve_timeout: 5m\n  http_config:\n    follow_redirects: true\n  smtp_hello: localhost\n  smtp_require_tls: true\n  slack_api_url: <secret>\n  pagerduty_url: https://events.pagerduty.com/v2/enqueue\n  opsgenie_api_url: https://api.opsgenie.com/\n  wechat_api_url: https://qyapi.weixin.qq.com/cgi-bin/\n  victorops_api_url: https://alert.victorops.com/integrations/generic/20131114/alert/\n  telegram_api_url: https://api.telegram.org\n',
+  },
+  cluster: {
+    name: '01G67PMHENCCK7W3XQR7FB5PH0',
+    peers: [
+      {
+        address: '10.233.1.1:9094',
+        name: '01G67P4NXKFYQZRZWZXX34W480',
+      },
+      {
+        address: '10.233.1.1:9094',
+        name: '01G67P7G5SA7Y99DY52M9ZWRVJ',
+      },
+      {
+        address: '10.233.1.1:9094',
+        name: '01G67PKGGPWDB97K2FHFAX7DXH',
+      },
+      {
+        address: '10.233.1.1:9094',
+        name: '01G67PMHENCCK7W3XQR7FB5PH0',
+      },
+    ],
+    status: 'ready',
+  },
+  versionInfo: {
+    branch: 'HEAD',
+    buildDate: '20220325-09:31:33',
+    buildUser: 'root@265f14f5c6fc',
+    goVersion: 'go1.17.8',
+    revision: 'f484b17fa3c583ed1b2c8bbcec20ba1db2aa5f11',
+    version: '0.24.0',
+  },
+};
+
+export const statusResult = {
+  uptime: new Date('2022-06-23T07:15:15.759Z'),
+  config: {
+    original:
+      'global:\n  resolve_timeout: 5m\n  http_config:\n    follow_redirects: true\n  smtp_hello: localhost\n  smtp_require_tls: true\n  slack_api_url: <secret>\n  pagerduty_url: https://events.pagerduty.com/v2/enqueue\n  opsgenie_api_url: https://api.opsgenie.com/\n  wechat_api_url: https://qyapi.weixin.qq.com/cgi-bin/\n  victorops_api_url: https://alert.victorops.com/integrations/generic/20131114/alert/\n  telegram_api_url: https://api.telegram.org\n',
+  },
+  cluster: {
+    name: '01G67PMHENCCK7W3XQR7FB5PH0',
+    peers: [
+      {
+        address: '10.233.1.1:9094',
+        name: '01G67P4NXKFYQZRZWZXX34W480',
+      },
+      {
+        address: '10.233.1.1:9094',
+        name: '01G67P7G5SA7Y99DY52M9ZWRVJ',
+      },
+      {
+        address: '10.233.1.1:9094',
+        name: '01G67PKGGPWDB97K2FHFAX7DXH',
+      },
+      {
+        address: '10.233.1.1:9094',
+        name: '01G67PMHENCCK7W3XQR7FB5PH0',
+      },
+    ],
+    status: 'ready',
+  },
+  versionInfo: {
+    branch: 'HEAD',
+    buildDate: '20220325-09:31:33',
+    buildUser: 'root@265f14f5c6fc',
+    goVersion: 'go1.17.8',
+    revision: 'f484b17fa3c583ed1b2c8bbcec20ba1db2aa5f11',
+    version: '0.24.0',
+  },
+};
--- test/alertmanager/alertmanager.spec.ts
+++ test/alertmanager/alertmanager.spec.ts
@@ -1,8 +1,9 @@
-import mockAxios from 'jest-mock-axios';
 import { Alertmanager } from '../../src';
+import mockAxios from '../../src/__mocks__/axios';
+import { statusResponse, statusResult } from './alertmanager.consts';
 
 describe('alertmanager', () => {
-  beforeEach(() => {
+  afterEach(() => {
     mockAxios.reset();
   });
 
@@ -16,7 +17,17 @@ describe('alertmanager', () => {
     expect(() => new Alertmanager('http:/localhost:9090')).toThrow();
   });
 
-  it('should return valid AlertmanagerStatus', () => {
-    // mockAxios.get.mockResolvedValue();
+  it('should return valid AlertmanagerStatus', async () => {
+    const amtool = new Alertmanager('http://localhost:9090');
+
+    const statusPromise = amtool.getStatus();
+
+    expect(mockAxios.get).toHaveBeenCalledWith('/api/v2/status');
+
+    mockAxios.mockResponse({ data: statusResponse, status: 200 });
+
+    const result = await statusPromise;
+
+    expect(result).toStrictEqual(statusResult);
   });
 });
